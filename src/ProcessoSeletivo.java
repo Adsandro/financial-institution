889bb80adf744551646916103e7634fd626b7c9f
Adsandro <adsandroxerd@gmail.com>
2023-09-05 19:28:58 -0300

    Implement selection candidate method

--- src/ProcessoSeletivo.java
+++ src/ProcessoSeletivo.java
@@ -1,7 +1,8 @@
+import java.util.concurrent.ThreadLocalRandom;
 public class ProcessoSeletivo {
     private String nomeCandidato;
     private float salarioDesejado;
-
+    float salarioBase = 2000;
     public ProcessoSeletivo(String nomeCandidato, float salarioDesejado){
         this.nomeCandidato = nomeCandidato;
         this.salarioDesejado = salarioDesejado;
@@ -20,13 +21,37 @@ public class ProcessoSeletivo {
     }
     public void analisaCandidato(){
         float salario = getSalarioDesejado();
-        float salarioBase = 2000;
-        if(salario > salarioBase){
+        if(salario > this.salarioBase){
             System.out.printf("Ligar para candidato e sugerir contraproposta!");
-        }else if(salarioBase == salario){
+        }else if(this.salarioBase == salario){
             System.out.printf("Ligar para candidato");
         }else{
             System.out.printf("Aguardar inscrição de novos candidatos");
         }
+    }
+    public static void selecaoCandidato(){
+        /*Recebe uma lista de candidatos*/
+        String[] candidatosSelecionados = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O"};
+
+        int candidatoAtual = 0;
+        int candidatoSelecionado = 0;
+        float salarioBase = 2000;
+
+        while(candidatoSelecionado < 5){
+            String candidato = candidatosSelecionados[candidatoAtual];
+            float salarioPretendido = valorPretendido();
+
+            System.out.printf("O candidato %s possui pretenção de R$ %f", candidato, salarioPretendido);
+            candidatoAtual = candidatoAtual+1;
+            if (salarioBase >= salarioPretendido){
+                candidatoSelecionado++;
+            }
+        };
+    };
+    static float valorPretendido(){
+        return (float) ThreadLocalRandom.current().nextDouble(1860,2200);
+    }
+    public static void main(String[] args){
+        selecaoCandidato();
     }
 }
